55493ea04ad4edcad85ea19a4c643400b55138eb
IanDBrown <ian@robertwbrown.net>
2024-03-02 15:54:01 -0500

    Added QuestionsPage.tsx

---
+++ frontend/src/components/QuestionPage.tsx
@@ -0,0 +1,27 @@
+const QuestionPage = () => {
+    
+    return ( 
+        <div>
+            <form>
+            <label>
+                <input type="radio" name="react-tips" value="option1" className="form-check-input"/>
+                Option 1
+            </label> <br/>
+            <label>
+                <input type="radio" name="react-tips" value="option2" className="form-check-input"/>
+                Option 2
+            </label> <br/>
+            <label>
+                <input type="radio" name="react-tips" value="option3" className="form-check-input"/>
+                Option 3
+            </label> <br/>
+            <label>
+                <input type="radio" name="react-tips" value="option4" className="form-check-input"/>
+                Option 4
+            </label> <br/>
+            </form>
+        </div>
+     );
+}
+ 
+export default QuestionPage;
