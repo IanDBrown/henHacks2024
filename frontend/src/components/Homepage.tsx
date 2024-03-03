0edaac124c84465e4defd497b00e738026b9014c
Hairum Qureshi <hqureshi@udel.edu>
2024-03-02 19:54:21 -0500

    test

--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -5,8 +5,17 @@ export default function Homepage() {
 		<div className="container">
 			<div className="title">
 				<h1>Intelli Learn</h1>
+				<div>
+					<div className="subjectSelect">
+						<select>
+							<option value="0">Select a subject:</option>
+							<option value="1">Reading</option>
+							<option value="2">Math</option>
+						</select>
+					</div>
+				</div>
 			</div>
-			<h1>An ethical AI!</h1>
+			<h1>An ethical homework helping AI!</h1>
 			<div className="imageContainer">
 				<img src={image} alt="Image of a green friendly robot" />
 			</div>
