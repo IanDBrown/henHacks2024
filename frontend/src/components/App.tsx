220a54a3204fc6ccbcdf6539450e2ddf15f35062
Hairum Qureshi <hqureshi@udel.edu>
2024-03-03 01:07:55 -0500

    re added button to homepage

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,23 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
-import AboutPage from "./AboutPage";  
-import NavBar from "./NavBar";  
+import AboutPage from "./AboutPage";
+import NavBar from "./NavBar";
 import HWHelperPage from "./HWHelperPage";
 import QuestionPage from "./QuestionPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <NavBar /> {/* NavBar is included here to ensure it's displayed on all pages */}
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/homework-helper" element={<HWHelperPage />} />
-        <Route path="/quiz" element={<QuestionPage />} />
-        {}
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<NavBar />{" "}
+			{/* NavBar is included here to ensure it's displayed on all pages */}
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/about" element={<AboutPage />} />
+				<Route path="/homework-helper" element={<HWHelperPage />} />
+				<Route path="/quiz" element={<QuestionPage />} />
+
+				{}
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
