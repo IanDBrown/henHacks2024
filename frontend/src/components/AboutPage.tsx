d50ce644ba9b75f8c269c913bf12c9391bbadf88
mohammed touheed patel <mohammedtouheedpatel2k15@gmail.com>
2024-03-03 11:01:18 -0500

    last minute error

--- frontend/src/components/AboutPage.tsx
+++ frontend/src/components/AboutPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import about_css from "../css/about.module.css";
 
 export default function AboutPage() {
