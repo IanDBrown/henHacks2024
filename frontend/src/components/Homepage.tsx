0e7286afb18f7ca9708bbf429a18193937c445b5
Hairum Qureshi <hqureshi@udel.edu>
2024-03-02 20:53:05 -0500

    fixed homepage

--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -2,7 +2,6 @@ import "../css/App.css";
 import image from "../assets/robot-image.png";
 
 export default function Homepage() {
-<<<<<<< HEAD
 	return (
 		<div className="container">
 			<div className="title">
@@ -16,27 +15,3 @@ export default function Homepage() {
 		</div>
 	);
 }
-=======
-    return (
-        <div className="container homepage">
-            <div className="title">
-                <h1>Intelli Learn</h1>
-                <div className="controls">
-                    <div className="subjectSelect">
-                        <select>
-                            <option value="0">Select a subject:</option>
-                            <option value="1">Reading</option>
-                            <option value="2">Math</option>
-                        </select>
-                    </div>
-                    <button>Take a test!</button> {/* Added the button here */}
-                </div>
-            </div>
-            <h1>An ethical homework helping AI!</h1>
-            <div className="imageContainer">
-                <img src={image} alt="Image of a green friendly robot" />
-            </div>
-        </div>
-    );
-}
->>>>>>> main
